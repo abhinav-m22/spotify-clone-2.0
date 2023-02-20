f2cbcd9062290f29402674480f32832b177dae3f
abhinav-m22 <abhinavpm05@gmail.com>
2023-02-20 19:27:17 +0530

    initial

---
+++ src/components/Error.jsx
@@ -0,0 +1,7 @@
+const Error = () => (
+  <div className="w-full flex justify-center items-center">
+    <h1 className='font-bold text-2xl text-white mt-2'>Something Went Wrong. Please Try Again</h1>
+  </div>
+);
+
+export default Error;
---
+++ src/pages/AroundYou.jsx
@@ -0,0 +1,49 @@
+// import React from 'react';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+
+const AroundYou = () => {
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+    const {activeSong, isPlaying} = useSelector((state) => state.player);
+    const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+
+    useEffect(() => {
+        axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_h5NxvTp4rB2xN8Y9ZL0KxYDegrXSx')
+        .then((res) => setCountry(res?.data?.location?.country))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
+    }, [country]);
+
+    if(isFetching && loading) return <Loader title="Loading Songs Around You..."/>
+
+    if(error && country !== '') return <Error />
+
+    return (
+        <div className='flex flex-col'>
+            <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>
+                Around You
+                <span className='font-black'> {country}</span>
+            </h2>
+
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {data?.map((song, i) =>(
+                    <SongCard 
+                        key = {song.key}
+                        song = {song}
+                        isPlaying = {isPlaying}
+                        activeSong = {activeSong}
+                        data = {data}
+                        i = {i}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default AroundYou;
