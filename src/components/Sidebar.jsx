f998791fcf91a22ca6f8fe192434ba7746242b82
abhinav-m22 <abhinavpm05@gmail.com>
2023-02-28 14:42:04 +0530

    updated logo and favicon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { HiOutlineMenu } from "react-icons/hi";
 import { RiCloseLine } from "react-icons/ri";
-import { logo } from "../assets";
 import { links } from "../assets/constants"
 
 const NavLinks = ({ handleClick }) => (
@@ -27,7 +26,7 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
       </div>
 
@@ -38,7 +37,7 @@ const Sidebar = () => {
       </div>
 
       <div className={`absolute top-0 h-screen w-2/5 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="logo" className="w-full h-14 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)}/>
       </div>
 
